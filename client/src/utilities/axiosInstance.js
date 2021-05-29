3cf2f4618e6aa07d6e866f9d4b4a75323646205a
Coder-Srinivas <srinivas.babu364@gmail.com>
2021-05-30 00:35:57 +0530

    Bug again

--- client/src/utilities/axiosInstance.js
+++ client/src/utilities/axiosInstance.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 const url = process.env.BACKEND_URL || "http://localhost:8000/";
 const axiosInstance = axios.create({
     withCredentials: true,
+    credentials: 'include',
     baseURL: url
 })
 
