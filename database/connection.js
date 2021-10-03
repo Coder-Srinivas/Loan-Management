73388a3f7b7060ab817a8c96b52e8db83f6d4fba
Coder-Srinivas <srinivas.babu364@gmail.com>
2021-10-03 22:39:06 +0530

    Fixed a silly error

--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const url = process.env.NODE_ENV === "production" ? 
-process.env.DEV_MONGO_URL : process.env.PROD_MONGO_URL;
+process.env.PROD_MONGO_URL : process.env.DEV_MONGO_URL;
 
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
